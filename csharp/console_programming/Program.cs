b8e722544e3edd92b700ff294e89942ae4273368
Beyza <bbasaga06@gmail.com>
2021-11-25 22:48:00 +0300

    Birinci dersin dosyalari eklenildi.

---
+++ csharp/console_programming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace giris
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("Adınızı giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı giriniz");
+            string surname = Console.ReadLine();
+            string yeni_isim= "Hoşgeldin" + " "+ name + " " + surname;
+            Console.WriteLine(yeni_isim);
+            
+
+        }
+    }
+}
