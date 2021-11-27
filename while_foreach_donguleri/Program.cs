f4c24747fdfe99c6156daeb0f3fb1d49445599de
Beyza <bbasaga06@gmail.com>
2021-11-27 22:53:03 +0300

    Dokuzuncu dersin dosyalari aktarildi.

---
+++ while_foreach_donguleri/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace while_foreach_donguleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("********while********\n");
+            //girilen sayiya kadar sayi dahil ortalamasini ekrana yazdir
+            Console.Write("bir sayi giriniz:");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi)           
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //a dan baslayarak z ye kadar tum harfleri ekrana yazdir
+            char character = 'a';
+            while (character<='z')
+            {
+                 Console.Write(character);
+                 character ++;
+            }
+
+            Console.Write("\n********foreach********\n");
+            string[] arabalar = {"BMV", "Toyota",  "Nissan", "Ford"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+                
+            }
+
+
+        }
+    }
+}
